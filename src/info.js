a6175bffbc54349bb3eaffb038b5a659ee902291
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-17 22:28:32 +0900

    8.2 useEffect
    컴포넌트가 랜더링될 때마다 특정 작업을 수행하도록 설정할수 있는 Hook입니다.

--- src/info.js
+++ src/info.js
@@ -1,8 +1,18 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 // 8.1.1 useState를 여러 번 사용하기
 const Info = () => {
     const [name, setName]  = useState('');
     const [nickname, setNickname] = useState('');
+
+    // 8.2 useEffect
+    // 컴포넌트가 랜더링될 때마다 특정 작업을 수행하도록 설정할수 있는 Hook입니다.
+    useEffect(() => {
+        console.log('랜더링이 완료되었습니다!');
+        console.log({
+            name,
+            nickname
+        });
+    });
 
     const onChangeName = e => {
         setName(e.target.value);
