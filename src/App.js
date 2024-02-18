59d5587196dd7803571cd3f982a6c16e588f7b34
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-18 13:21:10 +0900

    8.3.1 카운터 구현하기
    8.3.1 카운터 구현하기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-import Info from './info';
+// import Info from './info';
+import Counter from './Counter_reduce'
 
-function App() {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}>
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  )
+const App = () => {
+  return <Counter />
 }
+
+// function App() {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setVisible(!visible);
+//         }}>
+//         {visible ? '숨기기' : '보이기'}
+//       </button>
+//       <hr />
+//       {visible && <Info />}
+//     </div>
+//   )
+// }
 
 export default App;
